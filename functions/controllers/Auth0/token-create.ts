e482e861cfa44be9e4b9bc4466f207bee4feb1cd
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-08-03 01:16:22 -0700

    added Auth0 authentication to port 3000

--- functions/controllers/Auth0/token-create.ts
+++ functions/controllers/Auth0/token-create.ts
@@ -1,11 +1,17 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const _config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBASEURL: process.env.ISSUER_BASE_URL,
-  secret: process.env.SECRET,
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    secret: process.env.SECRET,
 };
+
+const checkAuthenticated = (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged In" : "Logged Out");
+};
+
+module.exports = { config, checkAuthenticated };
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,11 @@
 const functions = require("firebase-functions");
-const { helloWorld } = require("./controllers/helloworld/index");
+const express = require("express");
+const app = express();
+const { auth } = require("express-openid-connect");
+const { config, checkAuthenticated } = require("./controllers/Auth0/token-create.ts");
 
-exports.helloWorld = functions.https.onRequest(helloWorld);
+app.listen(3000);
+app.use(auth(config));
+app.get("/", checkAuthenticated);
+
+exports.checkAuthenticated = functions.https.onRequest(checkAuthenticated);
