d8d48dc71f71d69ccacd6026dff9b73ebb2d9496
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-08-06 18:57:29 -0700

    created test public/private routes

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,19 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
-const { auth } = require("express-openid-connect");
+const { auth, requiresAuth } = require("express-openid-connect");
 const { config, checkAuthenticated } = require("./controllers/Auth0/token-create.ts");
 
 app.listen(3000);
 app.use(auth(config));
 app.get("/", checkAuthenticated);
+
+app.get("/private", requiresAuth(), (req, res) => {
+  res.send("Hello private");
+});
+
+app.get("/public", (req, res) => {
+  res.send("Hello public");
+});
 
 exports.checkAuthenticated = functions.https.onRequest(checkAuthenticated);
