3a462b672bf48f1ea0b3e3dbc93103801336c2be
Krishna Pandian <krishnakpandian@gmail.com>
2021-08-10 18:57:17 -0700

    Testing on GH with new config

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 
 const auth0Config = functions.config().auth;
-const jwk_uri = auth0Config.issuer + ".well-known/jwks.json";
+const jwk_uri = auth0Config ? auth0Config.issuer + ".well-known/jwks.json" : ".well-known/jwks.json";
 
 module.exports = { auth0Config, jwk_uri };
