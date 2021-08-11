c3404ccf7fd95a5b729f4e324bce43f4d4915140
rvaidun <{ID}+{username}@users.noreply.github.com>
2021-08-10 17:24:13 -0700

    cors

--- functions/controllers/expressExample/index.js
+++ functions/controllers/expressExample/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+var corsOptions = {
+  origin: "https://www.cruzhacks.com/",
+  optionsSuccessStatus: 200,
+};
+
 const { addDocument } = require("../../utils/database");
 
-app.get("/", async (req, res) => {
+app.get("/", cors(corsOptions), async (req, res) => {
   try {
     addDocument("users", { test: "functions" });
     res.status(201).send({ code: 201, message: "Added data from db example" });
