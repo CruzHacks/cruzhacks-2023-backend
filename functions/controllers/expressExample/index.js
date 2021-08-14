e1405ad845108f9779475f2d4492769bf65a77eb
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-08-13 19:34:59 -0700

    completed email verification feature

--- functions/controllers/expressExample/index.js
+++ functions/controllers/expressExample/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const axios = require("axios");
+const functions = require("firebase-functions");
+
 const { addDocument } = require("../../utils/database");
 const { jwtCheck } = require("../../utils/middleware");
 const { corsConfig } = require("../../utils/config");
 
+const API_TOKENS = functions.config().tokens
+
 const app = express();
 app.disable("x-powered-by");
 app.use(helmet());
+app.use(express.json());
 
 const corsOptions = {
   origin: corsConfig,
@@ -25,8 +31,35 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/resend", jwtCheck, async (req, res) => {
+  try {
+    res.status(201).send({ message: "Hello?" });
+  } catch (error) {
+    res.status(500).send({ code: 500, message: "Bad/No key" });
+  }
+});
+
 app.post("/resend", jwtCheck, async (req, res) => {
-  res.status(201).send({ message: "private body reached" });
+  // this endpoint takes in a user_id then converts
+  // it into an API call
+  const options = {
+    // options for the API call
+    method: "POST",
+    url: "https://cruzhacks.us.auth0.com/api/v2/jobs/verification-email",
+    headers: { authorization: "Bearer " + API_TOKENS.email },
+    data: {
+      user_id: req.body.user_id,
+    },
+  };
+
+  // make API call then response
+  axios(options)
+    .then((axios_res) => {
+      res.status(201).json(axios_res.data);
+    })
+    .catch((axios_err) => {
+      res.status(500).json(axios_err);
+    });
 });
 
 module.exports = { app };
