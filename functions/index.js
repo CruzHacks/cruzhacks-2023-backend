6dd1dc0805c94d2a5ba71ea7e5174816464ef452
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-08-13 19:37:26 -0700

    completed email verification feature

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,4 @@ const { helloWorld } = require("./controllers/helloworld/index");
 const { app } = require("./controllers/expressExample/index");
 
 exports.helloWorld = functions.https.onRequest(helloWorld);
-
 exports.auth = functions.https.onRequest(app);
