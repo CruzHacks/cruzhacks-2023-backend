bda0ab583e5237e94c6248572e80e50a3bd33df9
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-08-18 20:37:18 -0700

    fixing linting

--- functions/__tests__/verifyRecaptcha/index.test.js
+++ functions/__tests__/verifyRecaptcha/index.test.js
@@ -1,78 +1,93 @@
-const testConfig = require("firebase-functions-test")();
 const supertest = require("supertest");
 const { verifyRecaptcha } = require("../../controllers/VerifyRecaptcha/index");
 jest.mock("isomorphic-fetch");
 const fetch = require("isomorphic-fetch");
-          
+
 describe("POST /submit", () => {
-    describe("Given no captcha token", () => {
-        it("should respond with a 401 status code", async () => {
-            const response = await supertest(verifyRecaptcha).post("/submit").send({});
-            expect(response.statusCode).toBe(401);
-        });
+  describe("Given no captcha token", () => {
+    it("should respond with a 401 status code", async () => {
+      const response = await supertest(verifyRecaptcha).post("/submit").send({});
+      expect(response.statusCode).toBe(401);
     });
-    describe("Given captcha token is an empty string", () => {
-        it("should respond with a 401 status code", async () => {
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "" });
-            expect(response.statusCode).toBe(401);
-        });
+  });
+  describe("Given captcha token is an empty string", () => {
+    it("should respond with a 401 status code", async () => {
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "" });
+      expect(response.statusCode).toBe(401);
     });
-    describe("Given captcha token is undefined", () => {
-        it("should respond with a 401 status code", async () => {
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: undefined });
-            expect(response.statusCode).toBe(401);
-        });
+  });
+  describe("Given captcha token is undefined", () => {
+    it("should respond with a 401 status code", async () => {
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: undefined });
+      expect(response.statusCode).toBe(401);
     });
-    describe("Given captcha token is null", () => {
-        it("should respond with a 401 status code", async () => {
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: null });
-            expect(response.statusCode).toBe(401);
-        });
+  });
+  describe("Given captcha token is null", () => {
+    it("should respond with a 401 status code", async () => {
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: null });
+      expect(response.statusCode).toBe(401);
     });
-    describe("Test cases with mocked fetching", () => {
-        afterEach(() => fetch.mockClear)
-        it("should respond with 500 status code with invalid response", async () => {
-            fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve() }));
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
-            expect(fetch).toHaveBeenCalledTimes(1);
-            expect(response.statusCode).toBe(500);
-        });
-
-        it("should respond with status code 200 with valid token and input secret", async () => {
-            fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve({"success": true}) }));
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
-            expect(fetch).toHaveBeenCalledTimes(2);
-            expect(response.statusCode).toBe(200);
-            expect(response.body.message).toBe("Successfully Validated Request");
-        });
-        it("should respond with status code 400 with invalid token and input secret", async () => {
-            fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve({"success": false, "error-codes": ["invalid-input-response", "invalid-input-secret"]}) }));
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
-            expect(fetch).toHaveBeenCalledTimes(3);
-            expect(response.statusCode).toBe(400);
-            expect(response.body.message).toBe("Invalid Token or Secret");
-        });
-        it("should respond with status code 400 with invalid token", async () => {
-            fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve({"success": false, "error-codes": ["invalid-input-response"]}) }));
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
-            expect(fetch).toHaveBeenCalledTimes(4);
-            expect(response.statusCode).toBe(400);
-            expect(response.body.message).toBe("Invalid Token");
-        });
-        it("should respond with status code 401 with invalid secret", async () => {
-            fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve({"success": false, "error-codes": ["invalid-input-secret"]}) }));
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
-            expect(fetch).toHaveBeenCalledTimes(5);
-            expect(response.statusCode).toBe(401);
-            expect(response.body.message).toBe("Invalid Secret" );
-        });
-        it("should respond with status code 400 with timeout or duplicate", async () => {
-            fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve({"success": false, "error-codes": ["timeout-or-duplicate"]}) }));
-            const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
-            expect(fetch).toHaveBeenCalledTimes(6);
-            expect(response.statusCode).toBe(400);
-            expect(response.body.message).toBe("Request timed out, or Duplicate key");
-        });
+  });
+  describe("Test cases with mocked fetching", () => {
+    afterEach(() => fetch.mockClear);
+    it("should respond with 500 status code with invalid response", async () => {
+      fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve() }));
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(response.statusCode).toBe(500);
     });
 
+    it("should respond with status code 200 with valid token and input secret", async () => {
+      fetch.mockReturnValue(Promise.resolve({ json: () => Promise.resolve({ success: true }) }));
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
+      expect(fetch).toHaveBeenCalledTimes(2);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toBe("Successfully Validated Request");
+    });
+    it("should respond with status code 400 with invalid token and input secret", async () => {
+      fetch.mockReturnValue(
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({ success: false, "error-codes": ["invalid-input-response", "invalid-input-secret"] }),
+        }),
+      );
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
+      expect(fetch).toHaveBeenCalledTimes(3);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("Invalid Token or Secret");
+    });
+    it("should respond with status code 400 with invalid token", async () => {
+      fetch.mockReturnValue(
+        Promise.resolve({
+          json: () => Promise.resolve({ success: false, "error-codes": ["invalid-input-response"] }),
+        }),
+      );
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
+      expect(fetch).toHaveBeenCalledTimes(4);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("Invalid Token");
+    });
+    it("should respond with status code 401 with invalid secret", async () => {
+      fetch.mockReturnValue(
+        Promise.resolve({
+          json: () => Promise.resolve({ success: false, "error-codes": ["invalid-input-secret"] }),
+        }),
+      );
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
+      expect(fetch).toHaveBeenCalledTimes(5);
+      expect(response.statusCode).toBe(401);
+      expect(response.body.message).toBe("Invalid Secret");
+    });
+    it("should respond with status code 400 with timeout or duplicate", async () => {
+      fetch.mockReturnValue(
+        Promise.resolve({
+          json: () => Promise.resolve({ success: false, "error-codes": ["timeout-or-duplicate"] }),
+        }),
+      );
+      const response = await supertest(verifyRecaptcha).post("/submit").send({ captcha: "token" });
+      expect(fetch).toHaveBeenCalledTimes(6);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("Request timed out, or Duplicate key");
+    });
+  });
 });
