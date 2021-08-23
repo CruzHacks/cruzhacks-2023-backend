59f0275fc501598b96d656ae0c5bc6174adc1ecb
rvaidun <{ID}+{username}@users.noreply.github.com>
2021-08-23 16:28:38 -0700

    removed unused import

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const { helloWorld } = require("./controllers/helloworld/index");
 const { app } = require("./controllers/expressExample/index");
-const { getM2MToken } = require("./controllers/retrieveM2MToken/index");
 exports.helloWorld = functions.https.onRequest(helloWorld);
 
 exports.auth = functions.https.onRequest(app);
