8e47456ea866ae1f7fcd48008179e5df352e5e14
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-08-24 01:14:50 -0700

    cleaned up code

--- functions/controllers/expressExample/index.js
+++ functions/controllers/expressExample/index.js
@@ -6,16 +6,12 @@ const axios = require("axios");
 const { addDocument } = require("../../utils/database");
 const { jwtCheck } = require("../../utils/middleware");
 const { corsConfig, issuer } = require("../../utils/config");
-const { db } = require("../../utils/admin");
+const { getM2MToken } = require("../../utils/m2m");
 
 const app = express();
 app.disable("x-powered-by");
 app.use(helmet());
 app.use(express.json());
-
-let api_collection = db.collection("tokencol"); // Waiting for Rahul to finish his sprint
-let api_doc = api_collection.doc("token");
-let api_token = api_doc.token;
 
 const api_url = issuer + "api/v2/";
 
@@ -39,25 +35,26 @@ app.post("/resend", jwtCheck, async (req, res) => {
   // this endpoint takes in a user_id then converts
   // it into an API call
   try {
+    const token = await getM2MToken();
     const checkIfVerifiedOptions = {
       // options for the API call
       // to see if email is already verified
       method: "GET",
       url: api_url + "users/" + req.user.sub,
-      headers: { authorization: "Bearer " + api_token },
+      headers: { authorization: "Bearer " + token },
     };
 
     const checkIfVerified = await axios(checkIfVerifiedOptions); // send request
-
     if (checkIfVerified.data.email_verified) {
       throw "Email Already Verified";
     }
+
     const sendVerificationEmail = {
       // options for the API call
       // to make call to auth0 server
       method: "POST",
       url: api_url + "jobs/verification-email",
-      headers: { authorization: "Bearer " + api_token },
+      headers: { authorization: "Bearer " + token },
       data: {
         user_id: req.user && req.user.sub ? req.user.sub : "",
       },
@@ -74,7 +71,7 @@ app.post("/resend", jwtCheck, async (req, res) => {
     if (err === "Email Already Verified") {
       res.status(406).send({ code: 406, message: "Email Already Verified" });
     } else {
-      res.status(500).send({ code: 500, message: "Unable to Send Verification Email" });
+      res.status(500).send({ code: 500, message: "Unable to Send Verification Email", err: err });
     }
   }
 });
