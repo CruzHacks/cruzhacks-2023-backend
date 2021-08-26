9add9df85d92742f9f0187c5909050c39b6781e9
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-08-26 14:34:09 -0700

    Adding middleware (#22)

--- functions/utils/middleware.js
+++ functions/utils/middleware.js
@@ -1,7 +1,11 @@
+/* eslint-disable consistent-return */
 var jwt = require("express-jwt");
 var jwks = require("jwks-rsa");
-const { audience, issuer, jwk_uri } = require("./config");
+const { audience, issuer, jwk_uri, apikey } = require("./config");
 
+/*
+Validates jwt and parses data into req.user
+*/
 const jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
     cache: true,
@@ -14,4 +18,31 @@ const jwtCheck = jwt({
   algorithms: ["RS256"],
 });
 
-module.exports = { jwtCheck };
+/*
+Additional Middleware for Non-Authenticated Requests
+Authentication field must be included with correct key
+You cannot use this Middleware with this jwtCheck
+*/
+const validKey = (req, res, next) => {
+  if (!req.headers.authentication || req.headers.authentication !== apikey || apikey === "") {
+    return res.status(403).send({ message: "Invalid Api Key" });
+  }
+  next();
+};
+
+/*
+Middleware that checks if custom API specified Auth0 permissions exist on a jwt
+Note: 
+These permissions are assigned by the Auth0 roles
+They are not for utilizing Auth0 Management Endpoints
+*/
+const hasPermission = (permission) => {
+  return (req, res, next) => {
+    if (!req.user || !req.user.permissions || !req.user.permissions.includes(permission)) {
+      return res.status(403).send({ message: "Invalid Permissions" });
+    }
+    next();
+  };
+};
+
+module.exports = { jwtCheck, validKey, hasPermission };
