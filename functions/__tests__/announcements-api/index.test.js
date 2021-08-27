34275780c382e663f347504012ea5c2bbe99932a
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-08-27 12:54:22 -0700

    minor fixes

--- functions/__tests__/announcements-api/index.test.js
+++ functions/__tests__/announcements-api/index.test.js
@@ -2,25 +2,25 @@ const supertest = require("supertest");
 const { announcements } = require("../../controllers/announcements-api/index");
 
 testConfig.mockConfig({
-    auth: {
-      cors: "site",
-      audience: "audience",
-      issuer: "issuer",
-      jwk_uri: "some uri",
-    },
+  auth: {
+    cors: "site",
+    audience: "audience",
+    issuer: "issuer",
+    jwk_uri: "some uri",
+  },
 });
 
 jest.mock("../../../utils/database");
 jest.mock("../../../utils/middleware");
 
 describe("tests announcements", () => {
-    describe("POST /announcements", () => {
+  describe("POST /announcements", () => {
 
-    });
-    describe("GET /announcements", () => {
+  });
+  describe("GET /announcements", () => {
 
-    });
-    describe("DELETE /announcements", () => {
+  });
+  describe("DELETE /announcements", () => {
 
-    });
+  });
 });
