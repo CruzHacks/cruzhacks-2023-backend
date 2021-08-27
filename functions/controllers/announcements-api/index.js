14ca798bba15a5cedfab51b6582acfa079de9cba
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-08-26 17:04:16 -0700

    initial commit for delete endpoint

--- functions/controllers/announcements-api/index.js
+++ functions/controllers/announcements-api/index.js
@@ -1,5 +1,10 @@
+const functions = require("firebase-functions");
+const { db } = require("./admin");
+const helmet = require("helmet");
 const express = require("express");
 const cors = require("cors");
+const { hasPermission } = require("../../utils/middleware");
+const { addDocument, queryDocument, deleteDocument } = require("../../utils/database")
 
 const announcements = express();
 const corsOptions = {
@@ -7,11 +12,20 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
+announcements.disable("x-powered-by");
+announcements.use(helmet());
 announcements.use(cors(corsOptions));
 announcements.use(express.json());
 
 // announcements.post('/')
-// announcements.delete('/:id')
-// announcements.get('/')
+
+
+announcements.delete("/announcements/:id", hasPermission, async (req, res) => {
+  if (deleteDocument("announcements", req.params.id)) {
+    return res.status(200).send({ error: false, status: 200, message: "successfully removed from database"})
+  }
+  return res.status(400).send({ error: true, status: 400, message: "unable to remove from database" })
+});
+
 
 module.exports = { announcements };
--- functions/utils/database.js
+++ functions/utils/database.js
@@ -8,4 +8,13 @@ const queryDocument = (collection, id) => {
   return db.collection(collection).doc(id).get();
 };
 
-module.exports = { addDocument, queryDocument };
+const deleteDocument = (collection, id) => {
+  const snapshot = queryDocument(collection, id);
+  if (snapshot.exists) {
+    snapshot.delete();
+    return true;
+  }
+  return false;
+};
+
+module.exports = { addDocument, queryDocument, deleteDocument };
