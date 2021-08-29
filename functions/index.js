d80f1f8d7551f46c367a02efc05392793f3095d9
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-08-28 21:23:03 -0700

    Removed Helloworld and changed expressExample name

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require("firebase-functions");
-const { helloWorld } = require("./controllers/helloworld/index");
-const { app } = require("./controllers/expressExample/index");
+const { app } = require("./controllers/endpoints/index");
 const { verifyRecaptcha } = require("./controllers/VerifyRecaptcha/index");
 
-exports.helloWorld = functions.https.onRequest(helloWorld);
 exports.auth = functions.https.onRequest(app);
 
 exports.verifyRecaptcha = functions.https.onRequest(verifyRecaptcha);
