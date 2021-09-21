b7df2c34c01dcc8d7f89c9ede698a214b34776f0
Krishna Pandian <krishnakpandian@gmail.com>
2021-09-21 00:04:33 -0700

    Fixing linter errors

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 const functions = require("firebase-functions");
 
 const function_name = process.env.FUNCTION_NAME || process.env.K_SERVICE;
-if (!function_name || function_name === 'auth') {
-    exports.auth = functions.https.onRequest(require("./controllers/auth/index").app);
+if (!function_name || function_name === "auth") {
+  exports.auth = functions.https.onRequest(require("./controllers/auth/index").app);
 }
-if (!function_name || function_name === 'verifyRecaptcha') {
-    exports.verifyRecaptcha = functions.https.onRequest(require("./controllers/VerifyRecaptcha/index").verifyRecaptcha);
+if (!function_name || function_name === "verifyRecaptcha") {
+  exports.verifyRecaptcha = functions.https.onRequest(require("./controllers/VerifyRecaptcha/index").verifyRecaptcha);
 }
-if (!function_name || function_name === 'announcements') {
-    exports.announcements = functions.https.onRequest(require("./controllers/announcements-api/index").announcements);
+if (!function_name || function_name === "announcements") {
+  exports.announcements = functions.https.onRequest(require("./controllers/announcements-api/index").announcements);
 }
