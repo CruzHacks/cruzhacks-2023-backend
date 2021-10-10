8dcc689590fd3c8fad3c88dfee04b1c9e7a5d515
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-10-09 21:35:34 -0700

    Feature/on write analytics (#36)
    * initial commit
    
    * fixes
    
    * linting fixes
    
    * removed unneccessary logger
    
    * bigDiff
    
    * conditional function exports

--- functions/index.js
+++ functions/index.js
@@ -8,3 +8,6 @@ if (!function_name || function_name === "verifyRecaptcha") {
 if (!function_name || function_name === "announcements") {
   exports.announcements = require("./controllers/announcements-api/index").service;
 }
+if (!function_name || function_name === "writeToAnalytics") {
+  exports.writeToAnalytics = require("./utils/analytics").service;
+}
