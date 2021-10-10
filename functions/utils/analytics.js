5558192e5e3e1384baf32407cc9d013adb1c36a1
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-10-09 21:35:34 -0700

    Feature/on write analytics (#36)
    * initial commit
    
    * fixes
    
    * linting fixes
    
    * removed unneccessary logger
    
    * bigDiff
    
    * conditional function exports

---
+++ functions/utils/analytics.js
@@ -0,0 +1,43 @@
+const functions = require("firebase-functions");
+const { db, admin } = require("./admin");
+
+/*
+    writeToAnalytics adds an event trigger to 'applicants' collection.
+    The event trigger updates a separate document's fields in the 'analytics' collection.
+    Fields include: 
+                -> total number of applicants
+                -> number of UCSC student applicants
+                -> number of first time hackers
+    **NOTE**
+    Fields are not mutually exclusive!
+    i.e. in the event of a document creation;
+            -> if an applicant were a first time hacker and a UCSC student:
+                --> ALL THREE FIELDS WILL BE INCREMENTED BY ONE
+                    
+*/
+
+const service = functions.firestore.document("applicants/{docId}").onCreate(async (snap) => {
+  const document = snap.data();
+  const analyticsDoc = db.collection("analytics").doc("applicant-analytics");
+  const toUpdate = {
+    ucscIncrement: document.ucscStudent ? 1 : 0,
+    firstTimeIncrement: document.firstCruzHack ? 1 : 0,
+    applicantIncrement: 1,
+  };
+  try {
+    await analyticsDoc.update({
+      applicantCount: admin.firestore.FieldValue.increment(toUpdate.applicantIncrement),
+      firstTimeCount: admin.firestore.FieldValue.increment(toUpdate.firstTimeIncrement),
+      ucscStudentCount: admin.firestore.FieldValue.increment(toUpdate.ucscIncrement),
+    });
+  } catch (error) {
+    functions.logger.error(error.message);
+    await analyticsDoc.set({
+      applicantCount: toUpdate.applicantIncrement,
+      firstTimeCount: toUpdate.firstTimeIncrement,
+      ucscStudentCount: toUpdate.ucscIncrement,
+    });
+  }
+});
+
+module.exports = { service };
