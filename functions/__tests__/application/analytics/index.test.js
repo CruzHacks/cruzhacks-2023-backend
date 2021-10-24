3d519d569654bc20a8b365ab9aac1e817301a0df
ssharm73 <ssharm73@ucsc.edu>
2021-10-23 19:44:28 -0700

    fixed test modules

--- functions/__tests__/application/analytics/index.test.js
+++ functions/__tests__/application/analytics/index.test.js
@@ -1,6 +1,7 @@
-const { queryDocument } = require("../../utils/database");
-const { jwtCheck, hasReadAnalytics } = require("../../utils/middleware");
+const { queryDocument } = require("../../../utils/database");
+const { jwtCheck, hasReadAnalytics } = require("../../../utils/middleware");
 const { makeDocumentSnapshot } = require("firebase-functions-test/lib/providers/firestore");
+const { application } = require("../../../functions/controllers/application/index");
 
 jwtCheck.mockImplementation((req, res, next) => {
   next();
@@ -19,10 +20,33 @@ describe("Analytics test", () => {
     });
     queryDocument.mockImplementationOnce(() => Promise.resolve(fakeDoc));
 
-    const res = await request(analytics).get("/");
+    const res = await request(application).get("/analytics");
     expect(jwtCheck).toHaveBeenCalledTimes(1);
     expect(hasReadAnalytics).toHaveBeenCalledTimes(1);
     expect(res.status).toBe(201);
+    done();
+  });
+  it("Should return no document", async (done) => {
+    const res = await request(application).get("/analytics");
+    expect(jwtCheck).toHaveBeenCalledTimes(1);
+    expect(hasReadAnalytics).toHaveBeenCalledTimes(1);
+    expect(res.status).toBe(404);
+    expect(res.body).toBe({ status: 404, message: "No Document" });
+    done();
+  });
+  it("Should return invalid permissions", async (done) => {
+    fakeDoc = makeDocumentSnapshot({
+      applicantCount: 100,
+      firstTimeCount: 100,
+      ucscStudentCount: 100,
+    });
+    queryDocument.mockImplementationOnce(() => Promise.resolve(fakeDoc));
+    
+    const res = await request(application).get("/analytics");
+    expect(jwtCheck).toHaveBeenCalledTimes(1);
+    expect(hasReadAnalytics).toHaveBeenCalledTimes(1);
+    expect(res.status).toBe(404);
+    expect(res.body).toBe({ status: 404, message: "No Document" });
     done();
   });
 });
