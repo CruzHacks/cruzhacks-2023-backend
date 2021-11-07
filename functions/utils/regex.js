403c95a3df4a6cd647336a73ed79a720215ef7de
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-11-06 22:46:49 -0700

    updated regex.js, updated application and announcements validation (#51)
    * updated regex.js, updated application and announcements validation
    
    * linter fix
    
    * unit tests
    
    * lint fix

--- functions/utils/regex.js
+++ functions/utils/regex.js
@@ -10,8 +10,18 @@ const alphanumericPunctuationRegex = (message) => {
   return !/^[\x20-\x7E]*$/.test(message);
 };
 
+const alphanumericPunctuationRegexWithNewLine = (message) => {
+  return !/^[\x20-\x7E\n]*$/.test(message);
+};
+
 const emailRegex = (email) => {
   return !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
 };
 
-module.exports = { phoneRegex, alphanumericPunctuationRegex, alphanumericRegex, emailRegex };
+module.exports = {
+  phoneRegex,
+  alphanumericPunctuationRegex,
+  alphanumericRegex,
+  alphanumericPunctuationRegexWithNewLine,
+  emailRegex,
+};
