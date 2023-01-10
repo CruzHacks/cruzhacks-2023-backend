082378f99c8f0b232307ef29ecb2a499be0ae301
Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
2023-01-09 21:00:14 -0800

    rsvp disabled at 1/9/2023 11:59 (#135)
    Co-authored-by: Sidhanta Sharma <ssharm73@myucsc.edu>

--- functions/controllers/hacker/index.js
+++ functions/controllers/hacker/index.js
@@ -111,6 +111,14 @@ hacker.post("/bulkCreateHackers", jwtCheck, hasCreateAdmin, async (req, res) => 
 
 hacker.put("/setAttendanceStatus", jwtCheck, hasUpdateHacker, async (req, res) => {
   try {
+    const lockoutDate = new Date(2023, 0, 10, 7, 59, 59); // UTC time
+    const currentDate = new Date();
+
+    if (currentDate.getTime() > lockoutDate.getTime()) {
+      res.status(500).send({ status: 500, error: "RSVP is locked out" });
+      functions.logger.error("Locked Out");
+      return;
+    }
     const confirmedStatus = req.body.confirmedStatus;
     if (confirmedStatus !== "CONFIRMED" && confirmedStatus !== "NOT ATTENDING") {
       console.log(confirmedStatus);
