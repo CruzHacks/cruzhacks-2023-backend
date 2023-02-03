4fb349fe4a6163631e5504300247e87e265b7bf9
Alperdec <49699829+Alperdec@users.noreply.github.com>
2023-02-02 18:40:14 -0800

    Deploy 02/02/23 (#173)
    * empty commit (#164)
    
    * Feature/no fcm announcements (#152)
    
    * updatestatus endpoint outline
    
    * live-announcements
    
    * edits
    
    * linting
    
    * new announcements working
    
    * fix linting errors
    
    * fix auth GET metadata validation
    
    * update yarn lockfile
    
    * add function spacing
    
    ---------
    
    Co-authored-by: Alper deCarion <adecario@ucsc.edu>
    
    * Bug/fix team rsvp (#165)
    
    * fixed rsvp for teams
    
    * removed metadata endpoint
    
    * fixed hanging functions
    
    * fixed rebase issues (#166)
    
    * Bug/lint on build error (#167)
    
    * updatestatus endpoint outline
    
    * fixes
    
    * revert fix
    
    * removed eol rule
    
    ---------
    
    Co-authored-by: Zachary Traczyk <cookies951@gmail.com>
    
    * Chore/function target update (#168)
    
    * updatestatus endpoint outline
    
    * fixes
    
    * revert fix
    
    * removed eol rule
    
    * added notify
    
    ---------
    
    Co-authored-by: Zachary Traczyk <cookies951@gmail.com>
    
    * Adding getHacker by ID for admin + fixed major bug (#172)
    
    * Bug/admin package (#175)
    
    * Adding getHacker by ID for admin + fixed major bug
    
    * Fixing package.json
    
    * forgot to put permissions back (#176)
    
    ---------
    
    Co-authored-by: Sidhanta Sharma <32908234+sidhantas@users.noreply.github.com>
    Co-authored-by: Zack Traczyk <zstraczyk324@gmail.com>
    Co-authored-by: Zachary Traczyk <cookies951@gmail.com>
    Co-authored-by: Devan O'Boyle <doboyle@ucsc.edu>

---
+++ functions/controllers/admin/index.js
@@ -0,0 +1,82 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { jwtCheck, hasReadAdmin } = require("../../utils/middleware");
+const { queryCollection, queryDocument, updateDocument } = require("../../utils/database");
+
+const admin = express();
+admin.disable("x-powered-by");
+admin.use(express.json());
+
+const auth0Config = functions.config().auth;
+const corsConfig = auth0Config ? auth0Config.cors : "";
+
+const corsOptions = {
+  origin: corsConfig,
+  optionsSuccessStatus: 200,
+};
+
+admin.use(cors(corsOptions));
+
+admin.get("/getHackers", jwtCheck, hasReadAdmin, async (req, res) => {
+  try {
+    const hackers = await queryCollection("Hackers");
+    const hackerDocs = [];
+    hackers.forEach((doc) => {
+      data = doc.data();
+      hackerDocs.push({
+        id: doc.id,
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        attendanceStatus: data.attendanceStatus,
+        checkedIn: data.checkedIn,
+      });
+    });
+
+    res.status(200).send({ status: 200, hackers: hackerDocs });
+  } catch (err) {
+    res.status(500).send({ status: 500, error: "Unable to retrieve hackers" });
+    functions.logger.log(`Unable to retrieve hacker document for EVERYONE,\nError: ${err}`);
+  }
+});
+
+admin.get("/getHacker/:id", jwtCheck, hasReadAdmin, async (req, res) => {
+  try {
+    const doc = await queryDocument("Hackers", req.params.id);
+    if (!doc.exists) {
+      functions.logger.error(`Could not fetch profile for ${req.params.id},\nError: Document does not exist`);
+      res.status(500).send({ status: 500, error: "No Hacker Profile" });
+      return;
+    }
+
+    const data = doc.data();
+    const hackerFields = {
+      id: doc.id,
+      firstName: data.firstName,
+      lastName: data.lastName,
+    };
+
+    res.status(200).send({ status: 200, hacker: hackerFields });
+  } catch (err) {
+    res.status(500).send({ status: 500, error: "Unable to retrieve hacker" });
+    functions.logger.log(`Unable to retrieve hacker document for ${req.params.id},\nError: ${err}`);
+  }
+});
+
+/**
+ * Does not take a request body
+ */
+admin.put("/checkIn/:id", jwtCheck, hasReadAdmin, async (req, res) => {
+  try {
+    await updateDocument("Hackers", req.params.id, { checkedIn: true });
+    res.status(201).send({ status: 201, message: "successful update" });
+  } catch (err) {
+    functions.logger.error(`${err}`);
+    res.status(500).send({ status: 500, error: `Error occurred in check in` });
+  }
+});
+
+const service = functions.https.onRequest(admin);
+
+module.exports = { admin, service };
