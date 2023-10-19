0d2178eee3f620e3b1ad2395b2a66a8c101c883f
Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>
2023-10-19 13:22:29 -0700

    add optional typescript support

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,40 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { service as authService } from "../controllers/auth/index";
+import { service as verifyRecaptchaService } from "../controllers/VerifyRecaptcha/index";
+import { service as announcementsService } from "../controllers/announcements-api/index";
+import { service as applicantsService } from "../controllers/applicants/index";
+import { service as applicationService } from "../controllers/application/index";
+import { service as analyticsService } from "../controllers/analytics/index";
+import { service as writeToAnalyticService } from "../controllers/analytics/index";
+import { service as hackerService } from "../controllers/hacker/index";
+import { service as adminService } from "../controllers/admin/index";
+import { service as cruzpointsService } from "../controllers/cruzpoints/index";
+import { service as teamsService } from "../controllers/teams/index";
+import { service as notifyService } from "../controllers/notify/index";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const auth = authService;
+const verifyRecaptcha = verifyRecaptchaService;
+const announcements = announcementsService;
+const applicants = applicantsService;
+const application = applicationService;
+const analytics = analyticsService;
+const writeToAnalytics = writeToAnalyticService;
+const hacker = hackerService;
+const admin = adminService;
+const cruzpoints = cruzpointsService;
+const teams = teamsService;
+const notify = notifyService;
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export {
+  auth,
+  verifyRecaptcha,
+  announcements,
+  applicants,
+  application,
+  analytics,
+  writeToAnalytics,
+  hacker,
+  admin,
+  cruzpoints,
+  teams,
+  notify,
+};
